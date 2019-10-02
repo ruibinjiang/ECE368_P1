d147ed3458dca5c564c59d9f8dca7355705c7412
ruibinjiang <jiang551@purdue.edu>
2019-10-01 20:07:20 -0400

    Update

--- sorting.c
+++ sorting.c
@@ -3,16 +3,12 @@
 #include "sorting.h"
 long *Load_File(char *Filename, int *Size){
     FILE *file_ptr = fopen(Filename, "r");
-    int longs_read = fscanf(file_ptr,"%d", Size);
+    fscanf(file_ptr,"%d", Size);
     long *file_array = malloc(sizeof(long) *  (*Size) );
-
+    int i;
     //Reading File
-    longs_read = fread(file_array, sizeof(long), (*Size), file_ptr);
-
-    if (longs_read != (*Size)){
-    printf("Error Reading Input File\n");
-    *Size = 0;
-    return NULL;
+    for (i = 0;i<*Size;i++){
+        fscanf(file_ptr,"%ld\n", file_array + i);
     }
 
     fclose(file_ptr);
@@ -21,19 +17,17 @@ long *Load_File(char *Filename, int *Size){
 }
 
 
-
-
-
-
-
-
 int Save_File(char *Filename, long *Array, int Size){
     FILE *file_ptr = fopen(Filename, "w");
-    int num_written = fprintf(file_ptr,"%d",Size);
+    int num_written = fprintf(file_ptr,"%d\n",Size);
     //Write to Array
-    
-    num_written = fwrite(Array, sizeof(long), Size, file_ptr);
-printf("Saved all %d long integers\n", num_written);
+    int i;
+    num_written = 0;
+    for (i = 0;i<Size;i++){
+        fprintf(file_ptr,"%ld\n", Array[i]);
+        num_written++;
+    }
+
     fclose(file_ptr);
 
     return num_written;
@@ -47,8 +41,39 @@ printf("Saved all %d long integers\n", num_written);
 
 
 void Shell_Insertion_Sort(long *Array, int Size, double *N_Comp, double *N_Move){
-    int seq_size = 0;
-    long *sequence = Gen_Seq1(Size, &seq_size);
+    int seq_size = 1;
+    long *sequence = malloc((sizeof(long)) * Size);
+
+    sequence[0] = 1;
+    int p2 = 0;
+    int p3 = 0;
+    
+    int i;
+    long u2 = 0;
+    long u3 = 0;
+    for (i=1;i<=Size;i++){
+        if (sequence[p2]*2 == sequence[i-1]){
+            ++p2;
+        }
+        if (sequence[p3]*3 == sequence[i-1]){
+            ++p3;
+        }
+        u2 = sequence[p2] *2;
+        u3 = sequence[p3] *3;
+        if(u2<u3){
+            ++p2;
+            sequence[i]=u2;
+        }
+        else{
+            ++p3;
+            sequence[i]=u3;
+        }
+        seq_size++;
+    }
+
+
+
+
 
     long temp_r = 0;
     long current_k = 0;
@@ -80,7 +105,7 @@ void Shell_Insertion_Sort(long *Array, int Size, double *N_Comp, double *N_Move)
 
 
 void Improved_Bubble_Sort(long *Array, int Size, double *N_Comp, double *N_Move){
-    int seq_size = 0;
+    int seq_size = 1;
     long *sequence = Gen_Seq1(Size, &seq_size);
 
     long temp_r = 0;
@@ -115,35 +140,7 @@ void Improved_Bubble_Sort(long *Array, int Size, double *N_Comp, double *N_Move)
 
 
 
-long * Gen_Seq1 (int N, int *seq_size){
-    long * seq1 = malloc(sizeof(long) * N);
-    seq1[0] = 1;
-    int p2 = 0;
-    int p3 = 0;
-    int i;
-    long u2 = 0;
-    long u3 = 0;
-    for (i=1;i<N;i++){
-        if (seq1[p2]*2 == seq1[i-1]){
-            p2++;
-        }
-        if (seq1[p3]*3 == seq1[i-1]){
-            p3++;
-        }
-        u2 = seq1[p2] *2;
-        u3 = seq1[p3] *3;
-        if(u2<u3){
-            p2++;
-            seq1[i]=u2;
-        }
-        else{
-            p3++;
-            seq1[i]=u3;
-        }
-        (*seq_size)=(*seq_size)+1;
-    }
-    return seq1;
-}
+
 
 
 
@@ -159,10 +156,57 @@ long * Gen_Seq2 (int N, int *seq_size){
     }
     return seq2;
 }
-/*
-void Save_Seq1 (char *Filename, int N){
 
+void Save_Seq1 (char *Filename, int N){
+    FILE *file_ptr = fopen(Filename, "w");
+    int seq_size = 1;
+    long *sequence = malloc((sizeof(long)) * N);
+
+    sequence[0] = 1;
+    int p2 = 0;
+    int p3 = 0;
+    
+    int i;
+    long u2 = 0;
+    long u3 = 0;
+    for (i=1;i<=N;i++){
+        if (sequence[p2]*2 == sequence[i-1]){
+            ++p2;
+        }
+        if (sequence[p3]*3 == sequence[i-1]){
+            ++p3;
+        }
+        u2 = sequence[p2] *2;
+        u3 = sequence[p3] *3;
+        if(u2<u3){
+            ++p2;
+            sequence[i]=u2;
+        }
+        else{
+            ++p3;
+            sequence[i]=u3;
+        }
+        seq_size++;
+    }
+
+    int i;
+    for (i = 0;i<seq_size;i++){
+        fprintf(file_ptr,"%ld\n", sequence[i]);
+    }
+    free(sequence);
+    fclose(file_ptr);
+    return;
 }
 void Save_Seq2 (char *Filename, int N){
+    FILE *file_ptr = fopen(Filename, "w");
+    int seq_size = 0;
+    long *sequence = Gen_Seq2(N, &seq_size);
 
-}*/
+    int i;
+    for (i = 0;i<seq_size;i++){
+        fprintf(file_ptr,"%ld\n", sequence[i]);
+    }
+
+    fclose(file_ptr);
+    return;
+}
